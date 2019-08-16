8b08441bcd0eb3d252c2f83d56d595eb6dc3c341
Steve Worley <sj.worley88@gmail.com>
2019-08-16 14:35:49 +1000

    Add redis safeguards.
    - Adds a check for an envvar to enable redis.
    - Adds a ping check to redis to determine if it is available.

--- bay/images/docker/settings.php
+++ bay/images/docker/settings.php
@@ -48,17 +48,36 @@ $config['environment_indicator.indicator']['bg_color'] = !empty($config['environ
 $config['config_split.config_split.local']['status'] = FALSE;
 
 // Redis.
-if (!drupal_installation_attempted()) {
-  $settings['redis.connection']['host'] = 'redis';
-  $settings['redis.connection']['port'] = '6379';
-  $settings['redis.connection']['password'] = '';
-  $settings['redis.connection']['base'] = 0;
-  $settings['redis.connection']['interface'] = 'PhpRedis';
-  $settings['cache']['default'] = 'cache.backend.redis';
-  $settings['cache']['bins']['bootstrap'] = 'cache.backend.chainedfast';
-  $settings['cache']['bins']['discovery'] = 'cache.backend.chainedfast';
-  $settings['cache']['bins']['config'] = 'cache.backend.chainedfast';
-  $settings['container_yamls'][] = $contrib_path . '/redis/example.services.yml';
+if (getenv('ENABLE_REDIS')) {
+  $redis_host = getenv('REDIS_HOST') ?: 'redis';
+  $redis_port = getenv('REDIS_PORT') ?: '6379';
+  try {
+    if (drupal_installation_attempted()) {
+      throw new \Exception('Drupal installation underway.');
+    }
+
+    $redis = new \Redis();
+    $redis->connect($redis_host, $redis_port);
+    $response = $redis->ping();
+
+    if (strpos($response, 'PONG') === 'FALSE') {
+      throw new \Exception('Redis reachable but is not responding correctly.');
+    }
+
+    $settings['redis.connection']['host'] = 'redis';
+    $settings['redis.connection']['port'] = '6379';
+    $settings['redis.connection']['password'] = '';
+    $settings['redis.connection']['base'] = 0;
+    $settings['redis.connection']['interface'] = 'PhpRedis';
+    $settings['cache']['default'] = 'cache.backend.redis';
+    $settings['cache']['bins']['bootstrap'] = 'cache.backend.chainedfast';
+    $settings['cache']['bins']['discovery'] = 'cache.backend.chainedfast';
+    $settings['cache']['bins']['config'] = 'cache.backend.chainedfast';
+    $settings['container_yamls'][] = $contrib_path . '/redis/example.services.yml';
+  }
+  catch (\Exception $error) {
+    // Use default cache settings.
+  }
 }
 
 // Expiration of cached pages on Varnish to 15 min
