0f4c17e42433d4a527d7ae61c8e021b6d55a602b
Steve Worley <sj.worley88@gmail.com>
2020-08-21 13:30:25 +1000

    Add supported tags support to the autobuild.

--- .circleci/scripts/trigger-dockerhub.sh
+++ .circleci/scripts/trigger-dockerhub.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 #
-# Trigger a dockerhub build.
+# Trigger a dockerhub build for all supported tags.
 #
 
+SUPPORTED_TAGS=(1.x 2.x edge)
+
 for ENDPOINT in "${!DOCKERHUB_TRIGGER_@}"; do
-  echo "[info]: Building $ENDPOINT"
-  code=$(curl --write-out "%{http_code}" --output /dev/null --silent -H "Content-Type: application/json" --data '{"docker_tag": "edge"}' -X POST "${!ENDPOINT}")
-  if [[ $code -ne 202 ]]; then
-    echo "[error]: Triggering a build was unsuccessful."
-  else
-    echo "[success]: Image build was triggered successfully."
-  fi
+  for TAG in "${SUPPORTED_TAGS[@]}"; do
+    echo "[info]: Building $TAG for $ENDPOINT"
+    code=$(curl --write-out "%{http_code}" --output /dev/null --silent -H "Content-Type: application/json" --data '{"docker_tag": "'$TAG'"}' -X POST "${!ENDPOINT}")
+    if [[ $code -ne 202 ]]; then
+      echo "[error]: Triggering a build was unsuccessful."
+    else
+      echo "[success]: Image build was triggered successfully."
+    fi
+  done
 done
