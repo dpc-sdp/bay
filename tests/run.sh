8d9735816b52c5940e4bc6394d1719919f1620ea
Steve <sj.worley88@gmail.com>
2020-08-25 10:03:16 +1000

    Add CI to 1.x (#10)

---
+++ tests/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+##
+## Run available goss tests.
+##
+
+BASEDIR=$(dirname "$0")
+fails=0
+
+for file in $BASEDIR/goss.*.yaml; do
+  prefix=$BASEDIR/goss.
+  service=${file/$prefix/}
+  service=${service/.yaml/}
+
+  echo "==> Running tests for $service..."
+  cp $file $PWD/goss.yaml
+  if ! dgoss run -i singledigital/$service:latest; then
+    fails=$((fails + 1))
+  fi
+  rm $PWD/goss.yaml
+done
+
+exit $fails
