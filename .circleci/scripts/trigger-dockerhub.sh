55517f403918fbb9a99be678907e8de6e9d84561
Steve Worley <sj.worley88@gmail.com>
2020-08-26 15:13:08 +1000

    Alignment with 1.x deploy process.

--- .circleci/scripts/trigger-dockerhub.sh
+++ .circleci/scripts/trigger-dockerhub.sh
@@ -4,16 +4,14 @@
 # Trigger a dockerhub build for all supported tags.
 #
 
-SUPPORTED_TAGS=(1.x 2.x)
-
 for ENDPOINT in "${!DOCKERHUB_TRIGGER_@}"; do
-  for TAG in "${SUPPORTED_TAGS[@]}"; do
-    echo "[info]: Building $TAG for $ENDPOINT"
-    code=$(curl --write-out "%{http_code}" --output /dev/null --silent -H "Content-Type: application/json" --data '{"docker_tag": "'$TAG'"}' -X POST "${!ENDPOINT}")
-    if [[ $code -ne 202 ]]; then
-      echo "[error]: Triggering a build was unsuccessful."
-    else
-      echo "[success]: Image build was triggered successfully."
-    fi
-  done
+  echo "[info]: Building $CIRCLE_BRANCH for $ENDPOINT"
+  # Build directly with $CIRCLE_BRANCH - this is controlled directly by
+  # the circle configuration which needs to be changed during upgrades.
+  code=$(curl --write-out "%{http_code}" --output /dev/null --silent -H "Content-Type: application/json" --data '{"docker_tag": "'$CIRCLE_BRANCH'"}' -X POST "${!ENDPOINT}")
+  if [[ $code -ne 202 ]]; then
+    echo "[error]: Triggering a build was unsuccessful."
+  else
+    echo "[success]: Image build was triggered successfully."
+  fi
 done
