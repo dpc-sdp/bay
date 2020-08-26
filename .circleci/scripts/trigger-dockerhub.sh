621814502192fbfb4a2cad8fd853f6a498582ee9
Steve <sj.worley88@gmail.com>
2020-08-26 15:42:01 +1000

    [2.x]: Add supported tags support to the autobuild. (#11)
    * Add supported tags support to the autobuild.
    
    * Removed edge from list of image tags to build.
    
    * Alignment with 1.x deploy process.
    
    Co-authored-by: Nick Santamaria <nicholas.santamaria@dpc.vic.gov.au>

--- .circleci/scripts/trigger-dockerhub.sh
+++ .circleci/scripts/trigger-dockerhub.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 #
-# Trigger a dockerhub build.
+# Trigger a dockerhub build for all supported tags.
 #
 
 for ENDPOINT in "${!DOCKERHUB_TRIGGER_@}"; do
-  echo "[info]: Building $ENDPOINT"
-  code=$(curl --write-out "%{http_code}" --output /dev/null --silent -H "Content-Type: application/json" --data '{"docker_tag": "edge"}' -X POST "${!ENDPOINT}")
+  echo "[info]: Building $CIRCLE_BRANCH for $ENDPOINT"
+  # Build directly with $CIRCLE_BRANCH - this is controlled directly by
+  # the circle configuration which needs to be changed during upgrades.
+  code=$(curl --write-out "%{http_code}" --output /dev/null --silent -H "Content-Type: application/json" --data '{"docker_tag": "'$CIRCLE_BRANCH'"}' -X POST "${!ENDPOINT}")
   if [[ $code -ne 202 ]]; then
     echo "[error]: Triggering a build was unsuccessful."
   else
