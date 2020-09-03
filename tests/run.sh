964de83e4f9a5a43b5390011e0bb9fabf4065387
Steve Worley <sj.worley88@gmail.com>
2020-09-03 11:28:03 +1000

    Add clamav image to bay.
    Run clamav unit tests.
    
    Properly increment fails in dgoss run.
    
    Update PHP goss tests.
    
    Update the dgoss run.
    
    Update goss runner.
    
    Update namespace.

--- tests/run.sh
+++ tests/run.sh
@@ -13,7 +13,11 @@ for file in $BASEDIR/goss.*.yaml; do
 
   echo "==> Running tests for $service..."
   cp $file $PWD/goss.yaml
-  dgoss run -i bay/$service:latest || fails++
+
+  if ! dgoss run -i singledigital/$service:latest; then
+    fails=$((fails+1))
+  fi
+
   rm $PWD/goss.yaml
 done
 
