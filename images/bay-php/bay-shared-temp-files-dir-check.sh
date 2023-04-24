7efdd8775920a32bfb8f3a5417c035202949de24
Guy Owen <guy.owen@dpc.vic.gov.au>
2023-04-24 16:21:51 +1000

    [DDS-1478] Fixed indentation
    Co-authored-by: Nick Santamaria <nicholas.santamaria@dpc.vic.gov.au>

--- images/bay-php/bay-shared-temp-files-dir-check.sh
+++ images/bay-php/bay-shared-temp-files-dir-check.sh
@@ -7,5 +7,5 @@ set -euo pipefail
 FILE_TEMP_PATH="/app/docroot/sites/default/files/private/tmp"
 if [[ ${BAY_SHARED_TEMP_FILES:-x} == "true" ]] && [[ ! -d $FILE_TEMP_PATH ]]; then
   echo "Missing file_temp_path - creating directory '$FILE_TEMP_PATH'"
-mkdir -p $FILE_TEMP_PATH || echo "fatal - unable to create directory $FILE_TEMP_PATH" && exit 1
+  mkdir -p $FILE_TEMP_PATH || echo "fatal - unable to create directory $FILE_TEMP_PATH" && exit 1
 fi
