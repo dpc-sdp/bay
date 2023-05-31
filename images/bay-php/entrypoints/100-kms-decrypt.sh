e41d2643f3f61d5ee377261f65318dae57b770cf
Steve Worley <sj.worley88@gmail.com>
2023-05-31 12:50:09 +1000

    Feat: Add KMS decrypt entrypoint. (#175)
    ---------
    
    Co-authored-by: Nick Santamaria <nicholas.santamaria@dpc.vic.gov.au>

---
+++ images/bay-php/entrypoints/100-kms-decrypt.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#/ Usage:
+#/ Description: Locate files in /app/keys and attempt to decrypt them using stored IAM account details.
+#/ Examples:
+#/ Requires:
+#/   AWS_ACCESS_KEY_ID
+#/   AWS_SECRET_ACCESS_KEY
+#/   AWS_DEFAULT_REGION
+#/ Options:
+#/   --help: Display this help message
+usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
+expr "$*" : ".*--help" > /dev/null && usage
+
+echoerr() { printf "%s\n" "$*" >&2 ; }
+info()    { echoerr "[INFO]    $*" ; }
+warning() { echoerr "[WARNING] $*" ; }
+error()   { echoerr "[ERROR]   $*" ; }
+fatal()   { echoerr "[FATAL]   $*" ; exit 1 ; }
+
+info "decrypting files"
+
+encrypted_files=""
+if [ -d "/app/keys" ]; then
+    encrypted_files=$(find /app/keys -type f -name "*.asc" -print0 2>/dev/null)
+fi
+
+if [ -n "$encrypted_files" ] && [ -n "$AWS_ACCESS_KEY_ID" ] && [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
+    for file in $encrypted_files; do
+        info " - ${file} > ${file%.asc}"
+        bay kms decrypt < "${file}" > "${file%.asc}" || error "unable to decrypt ${file}"
+    done
+else
+    info "no files to decrypt"
+fi
+
+# Set options back to previous state.
+set +eu
--- images/bay-php/entrypoints/bay-shared-temp-files-dir-check.sh
+++ images/bay-php/entrypoints/bay-shared-temp-files-dir-check.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-set -euo pipefail
+# set -euo pipefail
 ##
 # Check for the existence of the tmp files dir 'sites/default/files/private/tmp' and create if missing.
 #
 
 FILE_TEMP_PATH="/app/docroot/sites/default/files/private/tmp"
-if [[ ${BAY_SHARED_TEMP_FILES:-x} == "true" ]] && [[ ! -d $FILE_TEMP_PATH ]]; then
+
+if [ "${BAY_SHARED_TEMP_FILES:-x}" = "true" ] && [ ! -d "$FILE_TEMP_PATH" ]; then
   echo "Missing file_temp_path - creating directory '$FILE_TEMP_PATH'"
-  mkdir -p $FILE_TEMP_PATH || echo "fatal - unable to create directory $FILE_TEMP_PATH" && exit 1
+  mkdir -p "$FILE_TEMP_PATH" || echo "fatal - unable to create directory $FILE_TEMP_PATH" && exit 1
 fi
