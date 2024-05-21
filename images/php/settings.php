f860cfb96e5b1b365667a72a3ca9517e796c23c7
Guy Owen <guy.owen@dpc.vic.gov.au>
2024-05-21 10:16:27 +1000

    [SDPSUP-7185] Update search endpoints to use https

--- images/php/settings.php
+++ images/php/settings.php
@@ -319,7 +319,7 @@ $config['clamav.settings']['mode_daemon_tcpip']['port'] = $clamav_port;
 
 // Configure elasticsearch connections from environment variables.
 if (getenv('SEARCH_HASH') && getenv('SEARCH_URL')) {
-  $config['elasticsearch_connector.cluster.elasticsearch_bay']['url'] = sprintf('http://%s.%s', getenv('SEARCH_HASH'), getenv('SEARCH_URL'));
+  $config['elasticsearch_connector.cluster.elasticsearch_bay']['url'] = sprintf('https://%s.%s', getenv('SEARCH_HASH'), getenv('SEARCH_URL'));
 } else {
   $config['elasticsearch_connector.cluster.elasticsearch_bay']['url'] =  "http://elasticsearch:9200";
 }
