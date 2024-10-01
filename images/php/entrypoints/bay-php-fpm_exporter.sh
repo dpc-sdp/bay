7b288388941fc416f3334721bf1050fd807b4bd0
Nick Santamaria <nicholas.santamaria@dpc.vic.gov.au>
2024-10-01 16:41:15 +1000

    Updated script to latest version

--- images/php/entrypoints/bay-php-fpm_exporter.sh
+++ images/php/entrypoints/bay-php-fpm_exporter.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-echo starting php-fpm_exporter metrics server
-php-fpm_exporter server
+#/ Usage:       PHP_FPM_EXPORTER_ENABLED=true ./bay-php-fpm_exporter.sh
+#/ Options:
+#/   --help: Display this help message
+usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
+expr "$*" : ".*--help" > /dev/null && usage
+
+echoerr() { printf "%s\n" "$*" >&2 ; }
+info()    { echoerr "[INFO]    $*" ; }
+warning() { echoerr "[WARNING] $*" ; }
+error()   { echoerr "[ERROR]   $*" ; }
+fatal()   { echoerr "[FATAL]   $*" ; exit 1 ; }
+
+if [ "${PHP_FPM_EXPORTER_ENABLED:-false}" = "true" ]; then
+  info starting php-fpm_exporter metrics server
+  php-fpm_exporter server &
+else
+  info php-fpm_exporter metrics server disabled
+fi
